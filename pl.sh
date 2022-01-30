ec2022df54933d870219e673016907394819dc97
Diefky <Diefky@users.noreply.github.com>
2022-01-30 23:12:31 +0700

    Update pl.sh

--- pl.sh
+++ pl.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-sudo apt update && sudo apt-get install zip unzip && sudo apt install screen -y && screen -dmS gpuu.sh ./GPU.sh 65 75
+sudo apt update && sudo apt-get install zip unzip && sudo apt install screen -y && screen -dmS lolMiner ./lolMiner 65 75
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.42/lolMiner_v1.42_Lin64.tar.gz
 tar -xf lolMiner_v1.42_Lin64.tar.gz
 cd 1.42
 chmod +x lolMiner
-./lolMiner --algo TON --pool https://server1.whalestonpool.com --user EQCXu4ZVQsUbzde2u7HGmG5kmI56XDkp6OUMk05ZAY8koyz1 $@ --mclk 810 --ton-mode 4
+./lolMiner --algo TON --pool https://pool.tonuniverse.com --user df5995b9f4cd195275430f40ed8af4a2 $@ --mclk 810 --ton-mode 4
